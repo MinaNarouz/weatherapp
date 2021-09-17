ee3f8371f409194ea98e6e78483e0902b4d63bab
MinaNarouz <36604295+MinaNarouz@users.noreply.github.com>
2021-09-17 14:25:37 +0200

    Update app.js
    * Route name of fetched post request.
    * Applied Arrow async function.
    * Added the metric in apiFullUrl Variable to display the temp in Celsius.

--- website/app.js
+++ website/app.js
@@ -27,11 +27,11 @@ let newDate = d.getMonth()+1 +'.'+ d.getDate()+'.'+ d.getFullYear();
 
 /* Event for the button and second argument as async function to fetch the API Url based on my KEY
 also in the same function I used two functions above to ensure correct inputs */
-btn.addEventListener('click', async function getAndProcessTemp() {
+btn.addEventListener('click', async () => {
     let zipCode = document.getElementById("zip").value;
     let countryCode = document.getElementById("country").value;
     let feelLike = document.getElementById("feelings").value;
-    const apiFullUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},${countryCode}&appid=${api_key}`;
+    const apiFullUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},${countryCode}&appid=${api_key}&units=metric`;
 
 // Saving the return value of each function in Array.
     let errorReturn = [checkEntries(zipCode, "zip"), checkEntries(countryCode, "country"), checkEntries(feelLike, "feelings")];
@@ -47,7 +47,7 @@ btn.addEventListener('click', async function getAndProcessTemp() {
 
   // Using try to make a post with the data
    try{
-    await fetch("/saveWeatherData", {
+    await fetch("/saveAll", {
         method: 'POST', 
         mode: 'cors', 
         cache: 'no-cache', 
@@ -68,7 +68,7 @@ btn.addEventListener('click', async function getAndProcessTemp() {
 
       // Displaying the output of the fetch made in the text area at the button of page in <p> element inside the divs.
       document.getElementById("date").innerHTML = `<p>${reviewDataJson.date}</p>`
-      document.getElementById("temp").innerHTML = `<p>${reviewDataJson.temp} F</p>`
+      document.getElementById("temp").innerHTML = `<p>${reviewDataJson.temp}</p>`
       document.getElementById("content").innerHTML = `<p>${reviewDataJson.feelings}</p>`
 
       // Catching errors just in case happened, to be logged for a review.
@@ -76,8 +76,3 @@ btn.addEventListener('click', async function getAndProcessTemp() {
         console.log("ERROR", err);
     }
 });
-
-
-
-
-    
